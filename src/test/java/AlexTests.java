220ce9926641abc6c718e3fd7edfe6134c16897d
Maria Ukleeva <maria.ukleeva@gmail.com>
2023-04-14 17:55:08 +0300

    Добавлен класс Alex, юнит-тесты для классов Cat, Feline, Lion, Alex

---
+++ src/test/java/AlexTests.java
@@ -0,0 +1,58 @@
+import com.example.Alex;
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AlexTests {
+    @Test
+    public void alexHasManeReturnsTrue() throws Exception{
+        Feline feline = new Feline();
+        Alex alex = new Alex(feline);
+        boolean actualResult = alex.doesHaveMane();
+        Assert.assertEquals(true,actualResult);
+
+    }
+
+    @Test
+    public void alexGetFoodReturnsListOfMeat() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+        Alex alex = new Alex(feline);
+        List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> actualResult = alex.getFood();
+        assertTrue(actualResult.containsAll(expectedResult));
+    }
+
+    @Test
+    public void alexGetKittensShouldReturnZero() throws Exception{
+        Feline feline = new Feline();
+        Alex alex = new Alex(feline);
+        int actualResult = alex.getKittens();
+        assertEquals(0, actualResult);
+    }
+
+    @Test
+    public void alexGetFriendsReturnsListOfFriends() throws Exception {
+        Feline feline = new Feline();
+        Alex alex = new Alex(feline);
+        List<String> expectedResult = List.of("Марти", "Глория", "Мелман");
+        List<String> actualResult = alex.getFriends();
+        assertTrue(actualResult.containsAll(expectedResult));
+    }
+
+    @Test
+    public void alexGetPlaceOfLivingReturnsNewYorkZoo() throws Exception {
+        Feline feline = new Feline();
+        Alex alex = new Alex(feline);
+        String actualResult = alex.getPlaceOfLiving();
+        assertEquals("Нью-Йоркский зоопарк", actualResult);
+    }
+
+
+}
---
+++ src/test/java/CatTests.java
@@ -0,0 +1,37 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTests {
+    @Mock
+    Feline feline;
+
+
+    @Test
+    public void catEatMeatReturnsListOfMeat() throws Exception {
+        Cat cat = new Cat(feline);
+        List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> actualResult = cat.getFood();
+        assertTrue(actualResult.containsAll(expectedResult));
+    }
+
+    @Test
+    public void catGetSoundReturnsMeow(){
+        Feline feline = new Feline();
+        Cat cat = new Cat(feline);
+        String expectedResult = "Мяу";
+        String actualResult = cat.getSound();
+        assertEquals(expectedResult,actualResult);
+    }
+}
